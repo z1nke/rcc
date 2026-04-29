5898999f60f503813be0459c98fa72600dce4f12
z1nke <iamczn.cpp@gmail.com>
2026-04-29 17:41:03 +0800

    Add struct initializer for global variable

--- test/units/initializer.c
+++ test/units/initializer.c
@@ -8,6 +8,11 @@ short g4 = 4;
 int g5 = 5;
 long g6 = 6;
 
+// [106] Add struct initializer for global variable
+int g9[3] = {0, 1, 2};
+struct {char a; int b;} g11[2] = {{1, 2}, {3, 4}};
+struct {int a[2];} g12[2] = {{{1, 2}}};
+
 int main() {
   // [97] Support local variable initializers
   ASSERT(1, ({ int x[3]={1,2,3}; x[0]; }));
@@ -86,6 +91,21 @@ int main() {
   ASSERT(4, g4);
   ASSERT(5, g5);
   ASSERT(6, g6);
+
+  // [106] Add struct initializer for global variable
+  ASSERT(0, g9[0]);
+  ASSERT(1, g9[1]);
+  ASSERT(2, g9[2]);
+
+  ASSERT(1, g11[0].a);
+  ASSERT(2, g11[0].b);
+  ASSERT(3, g11[1].a);
+  ASSERT(4, g11[1].b);
+
+  ASSERT(1, g12[0].a[0]);
+  ASSERT(2, g12[0].a[1]);
+  ASSERT(0, g12[1].a[0]);
+  ASSERT(0, g12[1].a[1]);
 
   printf("OK\n");
   return 0;
